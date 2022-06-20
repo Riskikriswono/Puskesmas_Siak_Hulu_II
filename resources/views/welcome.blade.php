14f998d5273d1254cc46892a2fbfbc027107bdb8
Riski kriswono <riskikriswono@gmail.com>
2022-06-20 12:37:44 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,21 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-<div class="container-sm">
-<nav class="navbar bg-light">
-  <div class="container">
-    <a class="navbar-brand" href="#">
-      <img src="{{asset('img/logo.png')}}" alt="" width="731" height="120" class="container">
-    </a>
-  </div>
-  <div>hy</div>
-</nav>
-</div>
+    <div class="container-sm">
+        <nav class="navbar bg-light">
+            <div class="container">
+                <a class="navbar-brand" href="#">
+                    <img src="{{asset('img/logo.png')}}" alt="" width="731" height="120" class="container">
+                </a>jj
+            </div>
+            <div>hy</div>
+        </nav>
+    </div>
 </body>
+
 </html>
